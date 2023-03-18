8e331a2df1d33f9069b18ffc5a28419ec61fecc2
Mickael Salvas <mickael_salvas21@hotmail.com>
2023-03-18 11:07:23 -0400

    Initial commit

---
+++ src/main/java/tech/salvas/eifapi/EifApiApplication.java
@@ -0,0 +1,13 @@
+package tech.salvas.eifapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EifApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EifApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/salvas/eifapi/EifApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.salvas.eifapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EifApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
