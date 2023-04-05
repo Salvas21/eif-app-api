dcb907e9bfe280cea9b3fabdfafc9bd94e04ca30
Woontopia <xavloiselle@hotmail.com>
2023-04-05 18:12:42 -0400

    changed choice and attendance

--- src/main/java/tech/salvas/eifapi/models/Attendance.java
+++ src/main/java/tech/salvas/eifapi/models/Attendance.java
@@ -2,17 +2,37 @@ package tech.salvas.eifapi.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class Attendance {
     @Id
     private Long id;
-    private long activityId;
-    private long studentId;
+    @ManyToOne
+    private Activity activity;
+    @ManyToOne
+    private Student studentId;
     private String session = "";
     private boolean finished = false;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Attendance that = (Attendance) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/tech/salvas/eifapi/models/Choice.java
+++ src/main/java/tech/salvas/eifapi/models/Choice.java
@@ -2,21 +2,40 @@ package tech.salvas.eifapi.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Choice {
     @Id
     private Long id;
-    private long activityId;
-    private long studentId;
+    @ManyToOne
+    private Activity activity;
+    @ManyToOne
+    private Student studentId;
     private int preference;
     private LocalDateTime dateSubmitted = LocalDateTime.now();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Choice choice = (Choice) o;
+        return getId() != null && Objects.equals(getId(), choice.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
